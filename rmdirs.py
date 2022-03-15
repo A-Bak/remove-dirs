de0cdfc364c420b9c3a423b4a246ffe678aef6f8
Adam Bak <59940328+A-Bak@users.noreply.github.com>
2022-03-15 18:10:48 +0100

    Added - moving files. resolving file name conflicts after move.

--- rmdirs.py
+++ rmdirs.py
@@ -1,7 +1,9 @@
+from genericpath import samefile
 from tkinter.ttk import Separator
 from typing import List, Tuple
 
 import os
+import shutil
 import logging
 
 
@@ -12,34 +14,61 @@ FileName = str
 
 
 def remove(root_dir: FilePath, separator: chr='_') -> None:
-    
+    """ 
+    Docstring
+    """
     if not os.path.exists(root_dir):
         raise ValueError("Error: Invalid path to directory.")
     
-    for current_dir, _, files in os.walk(root_dir):
+    # First directory returned by os.walk() is the root_dir
+    # => Skip as there is no need to copy files
+    dir_iterator = os.walk(root_dir)
+    next(dir_iterator, None)
+    
+    for current_dir, _, files in dir_iterator:
         
         if files:
-            move_files(files, current_dir, root_dir, separator)
+            for file_name in files:
+                move_file(file_name, current_dir, root_dir, separator)
                 
         
-def move_files(files: List[FileName], current_dir: FilePath, root_dir: FilePath, separator: chr='_') -> None:
-    
-    # TODO Find new file name => (current_dir - root_dir) + file_name
-    # TODO Move/copy the file to root_dir
-    
-    for file_name in files:
-        
-        new_file_name = os.path.join(os.path.relpath(current_dir, start=root_dir),
-                                     file_name)
-        
-        backslashes = ['\\', '/']
-        
-        if contains(new_file_name, backslashes):
-            for ch in backslashes:
-                new_file_name = new_file_name.replace(ch, separator)
+def move_file(file_name: FileName, current_dir: FilePath, root_dir: FilePath, separator: chr='_') -> None:
+    """ 
+    Docstring
+    """
+    new_file_name = os.path.join(os.path.relpath(current_dir, start=root_dir),
+                                 file_name)
+    new_file_name = replace_chars(new_file_name, ['\\', '/'], separator)
 
-        logging.debug(f'Moved file {file_name}, new name {new_file_name}.')
+    source_file_path = os.path.join(current_dir, file_name)
+    target_file_path = os.path.join(root_dir, new_file_name)
+    
+    if not os.path.exists(target_file_path):
+        shutil.copyfile(source_file_path, target_file_path)
+        
+    else:
+        i = 1
+        file_path, extension = os.path.splitext(target_file_path)
+        
+        while os.path.exists(f'{file_path} ({i}){extension}'):
+            i += 1
+            
+        shutil.copy(source_file_path, f'{file_path} ({i}){extension}')
+        
+        logging.debug(f'New name is "{file_path} ({i}){extension}"')
+        
+        
+def replace_chars(file_name: FileName, char_list: List[chr], separator: chr='_') -> FileName:
+    """ Docstring """
+    new_file_name = file_name
+    
+    if contains(file_name, char_list):
+        for ch in char_list:
+            new_file_name = new_file_name.replace(ch, separator)
+
+    return new_file_name
 
 
 def contains(string: str, char_list: List[chr]) -> bool:     
+    """ Docstring """
     return any([ch in string for ch in char_list])
