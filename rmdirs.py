f03bfc107fed5cb7ba04e1857ec7077c2162a17b
Adam Bak <59940328+A-Bak@users.noreply.github.com>
2022-03-16 15:13:04 +0100

    Added - new_file_name(), rename_if_exists(), exception handling for string_contains() and replace_chars().

--- rmdirs.py
+++ rmdirs.py
@@ -7,68 +7,91 @@ import shutil
 import logging
 
 
-
+Char = str
 
 FilePath = str
 FileName = str
 
 
-def remove(root_dir: FilePath, separator: chr='_') -> None:
+def remove(root_dir: FilePath, separator: Char='_') -> None:
     """ 
     Docstring
     """
     if not os.path.exists(root_dir):
-        raise ValueError("Error: Invalid path to directory.")
+        raise ValueError("Invalid path to directory.")
     
     # First directory returned by os.walk() is the root_dir
     # => Skip as there is no need to copy files
-    dir_iterator = os.walk(root_dir)
-    next(dir_iterator, None)
+    dirtree_iter = os.walk(root_dir)
+    next(dirtree_iter, None)
     
-    for current_dir, _, files in dir_iterator:
+    for current_dir, _, files in dirtree_iter:
         
         if files:
             for file_name in files:
-                move_file(file_name, current_dir, root_dir, separator)
                 
-        
-def move_file(file_name: FileName, current_dir: FilePath, root_dir: FilePath, separator: chr='_') -> None:
+                source_file_path = os.path.join(current_dir, file_name)
+                target_file_path = new_file_name(file_name, current_dir, root_dir, separator)
+                
+                shutil.move(source_file_path, target_file_path)
+                
+                logging.debug(f'New name is "{target_file_path}".')
+    
+    
+def new_file_name(file_name: FileName, current_dir: FilePath, root_dir: FilePath, separator: Char='_'):
     """ 
     Docstring
     """
-    new_file_name = os.path.join(os.path.relpath(current_dir, start=root_dir),
-                                 file_name)
-    new_file_name = replace_chars(new_file_name, ['\\', '/'], separator)
-
-    source_file_path = os.path.join(current_dir, file_name)
-    target_file_path = os.path.join(root_dir, new_file_name)
+    new_file_name = os.path.join(os.path.relpath(current_dir, start=root_dir), file_name)
+    new_file_name = replace_chars(new_file_name, ['\\', '/'], separator)    
+    new_file_name = rename_if_exists(os.path.join(root_dir, new_file_name))
     
-    if not os.path.exists(target_file_path):
-        shutil.copyfile(source_file_path, target_file_path)
-        
-    else:
-        i = 1
-        file_path, extension = os.path.splitext(target_file_path)
-        
-        while os.path.exists(f'{file_path} ({i}){extension}'):
-            i += 1
-            
-        shutil.copy(source_file_path, f'{file_path} ({i}){extension}')
-        
-        logging.debug(f'New name is "{file_path} ({i}){extension}"')
-        
-        
-def replace_chars(file_name: FileName, char_list: List[chr], separator: chr='_') -> FileName:
-    """ Docstring """
-    new_file_name = file_name
-    
-    if contains(file_name, char_list):
-        for ch in char_list:
-            new_file_name = new_file_name.replace(ch, separator)
-
     return new_file_name
 
-
-def contains(string: str, char_list: List[chr]) -> bool:     
+        
+def rename_if_exists(target_file_path: FilePath) -> FilePath:
+    
+    if os.path.exists(target_file_path):
+        
+        # Split off extension from target_file_path and add index (i) to distinguish the file
+        # 'target/file/path.ext' -> 'target/file/path' + ' (i)' + .ext' ->  'target/file/path (i).ext'
+        
+        i = 1
+        new_file_path = '{1} ({0}){2}'.format(i, *os.path.splitext(target_file_path))
+        
+        while os.path.exists(new_file_path):
+            i += 1
+            new_file_path = '{1} ({0}){2}'.format(i, *os.path.splitext(target_file_path))
+        
+        return new_file_path
+                       
+    else:
+        return target_file_path    
+        
+        
+def replace_chars(string: str, char_list: List[Char], replacement: str='_') -> str:
     """ Docstring """
+    if not char_list:
+        raise ValueError('Argument char_list is an empty list.')
+    
+    new_string = string
+    
+    if string_contains(string, char_list):
+        for ch in char_list:
+            new_string = new_string.replace(ch, replacement)
+
+    return new_string
+
+
+def string_contains(string: str, char_list: List[Char]) -> bool:     
+    """ Docstring """
+    if not isinstance(string, str):
+        raise TypeError('String argument is not of type str.')
+    
+    if not char_list:
+        return True
+    
+    if any([not isinstance(ch, str) for ch in char_list]):
+        raise TypeError(('All elements of char_list must be of type str.'))
+    
     return any([ch in string for ch in char_list])
